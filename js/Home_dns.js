b7eeee88a3c40765d989fd8f8da8fda751f6a1b7
stanasischan <chenn0913@gmail.com>
2020-05-13 23:08:24 +0800

    Home_dns.js

--- js/Home_dns.js
+++ js/Home_dns.js
@@ -1,5 +1,23 @@
+var proxywifi = ["lovelifeasus","lovelifeasus_5G"];
+for (var i = 0; i < proxywifi.length; i++) {
+	if ($network.wifi.ssid==proxywifi[i]){		 $done({ server:'198.18.0.2' })
+	
+		break;
+		
+	};
+	if (i==proxywifi.length-1){
+	  $done({})
+	  };
+};
+$done();
+
+
+		
+
+/*#单wifi
 if ($network.wifi.ssid === 'lovelifeasus_5G') {
   $done({ server: '198.18.0.2' })
 } else {
   $done({})
 }
+*/
